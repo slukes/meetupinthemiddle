8b4f0a00a9385b2730330c872e450947908e053f
Sam Lukes <sam.lukes@yell.com>
2016-08-04 16:31:23 +0100

    Adding cookie bar

---
+++ src/main/resources/static/js/cookie.js
@@ -0,0 +1,44 @@
+$(function() {
+  $("#cookie-bar").on("click", "#close-cookie-bar", function () {
+    setConsentCookie();
+    $('#cookie-bar').hide();
+  });
+
+  getCookieConsent();
+});
+
+function getCookieConsent() {
+  var consent = hasConsentCookie();
+
+  if (!consent) {
+    // show bar
+    $('#cookie-bar').show();
+  }
+
+}
+
+function setConsentCookie() {
+  var expr = new Date();
+  expr.setFullYear(expr.getFullYear() + 1);
+  expr = expr.toUTCString();
+  document.cookie = "cookie-consent=true;" + "path=/; expires=" + expr;
+}
+
+function hasConsentCookie() {
+  var documentCookies = document.cookie.split(";");
+  for (var i = 0; i < documentCookies.length; i++) {
+    var eachCookieName = documentCookies[i]
+      .substr(0, documentCookies[i].indexOf("="));
+
+    if (eachCookieName.indexOf("cookie-consent") != -1) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+$("#cookie-bar").on("click", "#close-cookie-bar", function () {
+  setConsentCookie();
+  $('#cookie-bar').hide('slow');
+});
