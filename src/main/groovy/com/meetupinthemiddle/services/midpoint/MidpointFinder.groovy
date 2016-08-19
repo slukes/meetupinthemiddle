800d2a316fc0d4b9abc25ee0d8c1242c4aabbd10
Sam Lukes <sam.lukes@yell.com>
2016-08-19 15:45:49 +0100

    re-enabling cache

--- src/main/groovy/com/meetupinthemiddle/services/midpoint/MidpointFinder.groovy
+++ src/main/groovy/com/meetupinthemiddle/services/midpoint/MidpointFinder.groovy
@@ -1,8 +1,9 @@
 package com.meetupinthemiddle.services.midpoint
 import com.meetupinthemiddle.model.CentrePoint
 import com.meetupinthemiddle.model.Person
+import org.springframework.cache.annotation.Cacheable
 
 interface MidpointFinder {
-//  @Cacheable("midpoints")
+  @Cacheable("midpoints")
   Tuple2<CentrePoint, Map<Person, Long>> findMidpoint(List<Person> people);
 }
